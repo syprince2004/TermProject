1498d0c6649ced1f20b07312f8c91c04cea68415
박서연 <68016126+syprince2004@users.noreply.github.com>
2023-10-26 23:27:07 +0900

    현재층과 목표층을 비교하고 0.5초마다 다음층으로 넘어감
    #include <windows.h> Sleep() 함수를 사용해 0.5초마다 층이 증가 or 감소 되면서 다음층으로 넘어가는 것처럼 보임. 현재층과 목표층이 같아지면 while문을 빠져나와 엘리베이터가 도착했다는 문구를 프린트함.

--- el_feature.c
+++ el_feature.c
@@ -11,7 +11,9 @@
 
 #include <stdlib.h> //랜덤 함수를 쓰기 위한 헤더파일
 
-#include <time.h> //time() 함수를 쓰기 위한 헤더파일
+#include <time.h> //time 함수를 쓰기 위한 헤더파일
+
+#include <windows.h> //usleep 함수를 쓰기 위한 헤더파일
 
 /*
 랜덤으로 받는 현재 사용자 위치
@@ -22,13 +24,13 @@ return 값 : 램덤한값으로 받은 현재 사용자 위치
 라이브러리 :
 */
 
-int Rand_Now(int Posit)
+int Rand_Now(int currentFloor)
 {
 	srand(time(NULL)); //매번 다른 값 시드 생성
 
-	Posit = (rand() % 110) + 1; //1~110층 중 하나를 무작위로 받음
+	currentFloor = (rand() % 110) + 1; //1~110층 중 하나를 무작위로 받음
 
-	return(Posit);
+	return(currentFloor);
 }
 
 
@@ -43,16 +45,41 @@ return 값 : 램덤한값으로 받은 가고싶은 위치
 
 int Rand_future(int UD)
 {
-	int posit = Rand_Now(UD);
+	int currentFloor = Rand_Now(UD);
 	srand(time(NULL)); //매번 다른 값 시드 생성
-	int fu_posit = 0;
+	int targetFloor = 0;
 	if (UD == 1) {
-		fu_posit = (rand() % (110 - posit)) + posit; //현재 위치보다 높은 위치중 랜덤한 값
+		targetFloor = (rand() % (110 - currentFloor)) + currentFloor+1; //현재 위치보다 높은 위치중 랜덤한 값
 	}
 	else
-		fu_posit = (rand() % posit-1) + 1; //현재 위치보다 낮은 위치중 랜덤한 값
+		targetFloor = (rand() % currentFloor-1) + 1; //현재 위치보다 낮은 위치중 랜덤한 값
 
-	return(fu_posit);
+	double timePerFloor = 0.5; // 한 층 이동에 걸리는 시간 (초)
+
+	if (currentFloor == targetFloor) {
+		printf("엘리베이터는 이미 목표 층에 도착했습니다.\n"); //현재층과 목표층을 비교하여 같다면 문구 출력
+	}
+	else if (currentFloor < targetFloor) {
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor); //현재층이 목표층보다 작다면 
+		while (currentFloor < targetFloor) {
+			Sleep((int)(timePerFloor * 1000)); // usleep 함수를 사용하여 0.5초 대기
+			currentFloor++;
+			printf("현재 %d층\n", currentFloor);
+		}
+		printf("엘리베이터가 목표 층 %d에 도착했습니다.\n", targetFloor);
+	}
+	else {
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor);
+		while (currentFloor > targetFloor) {
+			Sleep((int)(timePerFloor * 1000)); // usleep 함수를 사용하여 대기
+			currentFloor--;
+			printf("현재 %d층\n", currentFloor);
+		}
+		printf("엘리베이터가 목표 층 %d에 도착했습니다.\n", targetFloor);
+	}
+
+
+	return(targetFloor);
 }
 
 /*
@@ -68,27 +95,10 @@ int UD_Button(int button)
 {
 	while (getchar() != '\n'); //문자 예외처리
 	if (button == 1 || button == 2) { 
-		int a = Rand_future(button); //받은값(가고싶은 방향)이 1 or 2 라면 Rand_futuer 함수 호출
-		printf("%d\n", a);
+		Rand_future(button); //받은값(가고싶은 방향)이 1 or 2 라면 Rand_futuer 함수 호출
 	}
 	else {
 		printf("error!\n"); //받고자하는 숫자가 아니라면 문구 출력
 		printf("다시입력하시오\n");
 	}
 }
-
-/*
-엘리베이터를 실시간으로 보여줌
-함수 목적 : 엘리베이터 관리 프로그램의 개발을 통한 프로그래밍 능력 향상
-return 값 : 사용자가 방향을 눌렀을때 가장 가까이 있는 엘리베이터가 옴
-개발 히스토리 :
-오픈소스 :
-라이브러리 :
-*/
-
-//int Elevator()
-//{
-//	printf("1 ");
-//	printf("1 ");
-//	for ()
-//}
