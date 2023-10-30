ce3d4009a8fa2cb35d08d36c64dbb2edbd9dd735
박서연 <68016126+syprince2004@users.noreply.github.com>
2023-10-30 21:30:36 +0900

    목표층을 입력받고 현재층에서 목표층으로 이동
    만약 위쪽 버튼를 눌렀을때 현재 위치가 50층이라면 목표층이 25층일때 어떻게 동작해야 하는가? 
    현재 엘리베이터는 위쪽 버튼을  눌렀을때 목표층(입력층)이 현재 층보다 낮다면 문이 닫혔다가 다시 열리고 닫히고 내려간다

--- el_feature.c
+++ el_feature.c
@@ -35,8 +35,8 @@ int Rand_Now(int currentFloor)
 
 
 /*
-랜덤으로 정하는 사용자가 가고자하는 위치
-함수 목적 : 사용자가 가고싶은 위치를 알려줌
+랜덤으로 정하는 사용자가 가고자하는 위치(목표층)
+함수 목적 : 목표층을 알려줌과 동시에 현재층과 목표층을 비교하면서 0.5초마다 다음 층으로 넘어가고 화면에 보여줌
 return 값 : 램덤한값으로 받은 가고싶은 위치
 개발 히스토리 :
 오픈소스 :
@@ -45,7 +45,8 @@ return 값 : 램덤한값으로 받은 가고싶은 위치
 
 int Rand_future(int UD)
 {
-	int currentFloor = Rand_Now(UD);
+	/* 
+	//목표층을 랜덤으로 구하는 코드
 	srand(time(NULL)); //매번 다른 값 시드 생성
 	int targetFloor = 0;
 	if (UD == 1) {
@@ -53,27 +54,61 @@ int Rand_future(int UD)
 	}
 	else
 		targetFloor = (rand() % currentFloor-1) + 1; //현재 위치보다 낮은 위치중 랜덤한 값
+	*/
+
+	int currentFloor = Rand_Now(UD);
+
+	int targetFloor = 0;
+
+	int initialFloor = 1;
 
 	double timePerFloor = 0.5; // 한 층 이동에 걸리는 시간 (초)
+
+	if (initialFloor == targetFloor) {
+		printf("엘리베이터는 이미 목표 층에 도착했습니다.\n"); //초기층과 현재층을 비교하여 같다면 문구 출력
+	}
+	else if (initialFloor < currentFloor) {
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", initialFloor, currentFloor); //초기층이 현재층보다 작다면 초기층에서 현재층으로 증가
+		while (initialFloor < currentFloor) {
+			Sleep((int)(timePerFloor * 1000)); // Sleep 함수를 사용하여 0.5초 대기
+			initialFloor++; //
+			printf("현재 %d층\n", initialFloor); //숫자를 하나씩 증가해주면서 층 보여줌
+		}
+		printf("엘리베이터가 사용자 층 %d에 도착했습니다.\n", currentFloor);
+		currentFloor = initialFloor; //다시 입력 받는 경우 초기 엘리베이터의 위치가 달라짐
+	}
+	else {
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", initialFloor, currentFloor); //초기층이 현재층보다 크다면 초기층에서 현재층으로 증가
+		while (initialFloor > currentFloor) {
+			Sleep((int)(timePerFloor * 1000)); // Sleep 함수를 사용하여 대기
+			initialFloor--;
+			printf("현재 %d층\n", initialFloor); //숫자를 하나씩 감소해주면서 층 보여줌
+		}
+		printf("엘리베이터가 목표 층 %d에 도착했습니다.\n", currentFloor);
+		currentFloor = initialFloor; //다시 입력 받는 경우 초기 엘리베이터의 위치가 달라짐
+	}
+
+	printf("가고자하는 층을 입력하시오 > ");
+	scanf_s("%d", &targetFloor);
 
 	if (currentFloor == targetFloor) {
 		printf("엘리베이터는 이미 목표 층에 도착했습니다.\n"); //현재층과 목표층을 비교하여 같다면 문구 출력
 	}
 	else if (currentFloor < targetFloor) {
-		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor); //현재층이 목표층보다 작다면 
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor); //현재층이 목표층보다 작다면 현재층에서 목표층으로 증가
 		while (currentFloor < targetFloor) {
-			Sleep((int)(timePerFloor * 1000)); // usleep 함수를 사용하여 0.5초 대기
-			currentFloor++;
-			printf("현재 %d층\n", currentFloor);
+			Sleep((int)(timePerFloor * 1000)); // Sleep 함수를 사용하여 0.5초 대기
+			currentFloor++; //
+			printf("현재 %d층\n", currentFloor); //숫자를 하나씩 증가해주면서 층 보여줌
 		}
 		printf("엘리베이터가 목표 층 %d에 도착했습니다.\n", targetFloor);
 	}
 	else {
-		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor);
+		printf("엘리베이터가 %d층에서 %d층으로 이동합니다.\n", currentFloor, targetFloor); //현재층이 목표층보다 크다면 현재층에서 목표층으로 증가
 		while (currentFloor > targetFloor) {
-			Sleep((int)(timePerFloor * 1000)); // usleep 함수를 사용하여 대기
-			currentFloor--;
-			printf("현재 %d층\n", currentFloor);
+			Sleep((int)(timePerFloor * 1000)); // Sleep 함수를 사용하여 대기
+			currentFloor--; 
+			printf("현재 %d층\n", currentFloor); //숫자를 하나씩 감소해주면서 층 보여줌
 		}
 		printf("엘리베이터가 목표 층 %d에 도착했습니다.\n", targetFloor);
 	}
@@ -91,7 +126,7 @@ return 값 : 사용자가 방향을 눌렀을때 가장 가까이 있는 엘리베이터가 옴
 라이브러리 :
 */
 
-int UD_Button(int button)
+void UD_Button(int button)
 {
 	while (getchar() != '\n'); //문자 예외처리
 	if (button == 1 || button == 2) { 
@@ -102,3 +137,9 @@ int UD_Button(int button)
 		printf("다시입력하시오\n");
 	}
 }
+
+
+
+
+
+/*현재 문제점 초기 엘리베이터 층을 1층으로 초기화 함. 하지만 두번째 버튼을 입력했을때 전에 끝냈던 목표층으로 끝냈던 초기값으로 다시 시작을 해야함.. 또한 주석을 쓸때 */
